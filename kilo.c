bc9713b064568176d401f60dcf189c5c72c1b194
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-04-07 14:03:48 +0300

    chapter 1: printing characters with their byte(s) value(s) in terminal

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -14,7 +16,7 @@ void enableRawMode() {
 
 	struct termios raw = orig_termios;
 
-	raw.c_lflag &= ~(ECHO);
+	raw.c_lflag &= ~(ECHO | ICANON);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -23,6 +25,12 @@ int main() {
 	enableRawMode();
 
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else {
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }
