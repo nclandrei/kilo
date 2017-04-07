c2817de22096cc6cde6970cf27402df862d6a0e4
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-04-07 14:26:10 +0300

    chapter 2: refactoring

--- kilo.c
+++ kilo.c
@@ -7,6 +7,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -44,24 +48,36 @@ void enableRawMode() {
 	}
 }
 
+char editorReadKey() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) {
+			die("read");
+		}
+	}
+	return c;
+}
+
+/*** input ***/
+
+void editorProcessorKeypress() {
+	char c = editorReadKey();
+
+	switch(c) {
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+}
+
 /*** init ***/
 
 int main() {
 	enableRawMode();
 
 	while (1) {
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-			die("read");
-		}
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == 'q') {
-			break;
-		}
+		editorProcessorKeypress();
 	}
 	return 0;
 }
