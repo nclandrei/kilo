05ff2a4e8ad4e347cfdf0db86b30af288f52a01c
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-04-07 14:14:55 +0300

    chapter 1: removing extra flags; exitting if no input for some time

--- kilo.c
+++ kilo.c
@@ -16,9 +16,12 @@ void enableRawMode() {
 
 	struct termios raw = orig_termios;
 
-	raw.c_lflag &= ~(ICRNL | IXON);
+	raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_lflag &= ~(OPOST);
-	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+	raw.c_lflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -26,12 +29,16 @@ void enableRawMode() {
 int main() {
 	enableRawMode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	while (1) {
+		char c = '\0';
+		read(STDIN_FILENO, &c, 1);
 		if (iscntrl(c)) {
 			printf("%d\r\n", c);
 		} else {
 			printf("%d ('%c')\r\n", c, c);
+		}
+		if (c == 'q') {
+			break;
 		}
 	}
 	return 0;
